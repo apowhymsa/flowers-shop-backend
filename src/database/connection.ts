9d824ddfa94fb4bc20bec635e3f9a80088150f50
whymsa <apowhymsa@gmail.com>
2023-11-04 13:09:08 +0200

    added user schema and model functions

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,5 +2,5 @@ import mongoose from 'mongoose';
 import * as process from "process";
 
 mongoose.connect(process.env['MONGO_URI'])
-    .then(() => console.log('MongoDB connected'))
+    .then(() => import('./schemes/users'))
     .catch((error: Error) => console.error(`Error: ${error.message}`));
